3c48af0ff41fa432b279d4f8185bdd7240ab1744
Salinas Matias <salinasmatiasit@gmail.com>
2025-05-08 04:40:15 -0300

    @get @delete @put /detalle_pedido

---
+++ src/models/DetallePedido.model.ts
@@ -0,0 +1,29 @@
+import pool from "../db/connection";
+import { DetallePedido } from "../interfaces/producto.interface";
+
+export class DetallePedidoModel {
+    
+    static async getDetallePedidoById(id: number) : Promise <DetallePedido | null> {
+        try {
+            const sql = "SELECT * FROM detalle_pedido WHERE id_detalle_pedido = ?;"
+            const [row] = await pool.execute(sql,[id]);
+            const detallePedido = (row as DetallePedido[]);
+            return detallePedido.length < 0 ? null : detallePedido[0];
+        } catch (error) {
+            throw new Error(`Error al `)
+        }
+      
+    }
+
+    static async updateDetallePedido(id: number, detalle:Partial <DetallePedido>) : Promise <boolean>{
+        const entries = Object.entries(detalle).filter(([key])=> key !== "id");
+        if (entries.length === 0){
+            return false
+        }
+        const setClause = entries.map(([key]) => `${key} = ?`).join(', ');
+        const values = entries.map(([, key])=> key);
+        const sql = `UPDATE detalle_pedido SET ${setClause} WHERE id_producto = ?;`;
+        const [result] = await pool.execute(sql,[...values,id]);
+        return (result as any).affectedRows > 0;
+    } 
+}
