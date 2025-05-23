cafd5557e35ba8065a14a28f1575d4f9f8fb247c
Salinas Matias <salinasmatiasit@gmail.com>
2025-05-23 03:21:49 -0300

    DTO para pedidos, funcion calcular precio variante en el modelo producto

---
+++ src/interfaces/pedidos.interface.ts
@@ -0,0 +1,7 @@
+export interface DetallePedidoCalculado {
+    producto_id: number;
+    variacion_id: number;
+    cantidad: number;
+    precio_unitario: number;
+    subtotal: number;
+}
--- src/models/Pedido.model.ts
+++ src/models/Pedido.model.ts
@@ -92,4 +92,13 @@ export class PedidoModel {
             const [row] = await pool.execute(sql,[id]);
             return (row as any).affectedRows > 0;
     }
-}
+
+    static async findPrecioVariante(id_producto: number, id_variante: number ) : Promise<number>{
+        const sql = `SELECT (precio_adicional + precio) AS sub_total FROM productos p 
+        INNER JOIN variaciones_productos vp ON p.id_producto = vp.id_producto 
+        WHERE p.id_producto = ? AND pv.id_variaciones_producto = ?`
+
+        const [result] = await pool.execute(sql,[id_producto,id_variante])
+        return (result as any).sub_total;
+    }
+ }
